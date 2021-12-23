09f2c3148c25ffce10d8058e1eb543884b75e5b5
Anastasia <asya_apto@mail.ru>
2021-12-24 00:09:59 +0300

    Исправила draw()

--- draw.py
+++ draw.py
@@ -1,20 +1,20 @@
 import random
+
 from models import GameMember, Match, Game
 
 
-games = Game.select()
-for game in games:
-    users = GameMember.select().where(GameMember.game=game.id)
+def draw():
+    games = Game.select()
+    for game in games:
+        users = GameMember.select().where(GameMember.game=game.id)
 
-    random.shuffle(users)
-    for i in range(len(users)):
-        giver = users[i]
-        recipient = users[(i+1) % (len(users))]
+        random.shuffle(users)
+        for i in range(len(users)):
+            giver = users[i]
+            recipient = users[(i+1) % (len(users))]
 
-        print(f"{giver} дарит {recipient}")
-
-        Match.create(
-            game = game.id,
-            giver = giver.id,
-            recipient = giver.id,
-        )
+            Match.create(
+                game = game.id,
+                giver = giver.id,
+                recipient = recipient.id,
+            )
