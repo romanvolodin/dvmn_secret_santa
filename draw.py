0081cbcc771eed4b76f1d0e6da2d458b10df2730
Anastasia <asya_apto@mail.ru>
2021-12-24 23:49:35 +0300

    Разнесла ф-цию жеребьевки на автоматическую и ручную

--- draw.py
+++ draw.py
@@ -3,18 +3,26 @@ import random
 from models import GameMember, Match, Game
 
 
-def draw():
+def draw(users):
+    random.shuffle(users)
+    for i in range(len(users)):
+        giver = users[i]
+        recipient = users[(i + 1) % (len(users))]
+        Match.create(
+            game=game.id,
+            giver=giver.id,
+            recipient=recipient.id,
+        )
+
+
+def automatic_draw():
     games = Game.select()
     for game in games:
-        users = GameMember.select().where(GameMember.game=game.id)
+        users = GameMember.select().where(GameMember.game == game.id)
+        draw(users)
+        
 
-        random.shuffle(users)
-        for i in range(len(users)):
-            giver = users[i]
-            recipient = users[(i+1) % (len(users))]
+def manual_draw(game_id):
+    users = GameMember.select().where(GameMember.game == game_id)
+    draw(users)
 
-            Match.create(
-                game = game.id,
-                giver = giver.id,
-                recipient = recipient.id,
-            )
