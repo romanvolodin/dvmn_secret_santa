1c889dcbbe6a79687849a9e67fd3813ca616cac2
Anastasia <asya_apto@mail.ru>
2021-12-26 17:03:32 +0300

    Добавила в рассылку описание условий игры

--- draw.py
+++ draw.py
@@ -43,20 +43,23 @@ def send_contacts(game_id):
     for match in all_matches:
         giver_id = match.giver_id
         recipient_id = match.recipient_id
-        game_title = Game.get(Game.id == game_id).title
-        message = create_msg(recipient_id, game_title)
+        game = Game.get(Game.id == game_id)
+        message = create_msg(recipient_id, game)
         bot.sendMessage(chat_id=giver_id, text=message)
 
 
-def create_msg(recipient_id, game_title):
+def create_msg(recipient_id, game):
     recipient = User.get_by_id(recipient_id)
     text = (
-        f"Жеребьевка в игре “{game_title}” проведена! Спешу сообщить кто тебе выпал:\n"
+        f"Жеребьевка в игре “{game.title}” проведена! Спешу сообщить кто тебе выпал:\n"
         f"• Имя получателя: {recipient.name}\n"
         f"• Email: {recipient.email}\n"
         f"• Пожелания: {recipient.wishlist}\n"
         f"• Интересы: {recipient.interests}\n"
-        f"• Записка для Санты: {recipient.letter}"
+        f"• Записка для Санты: {recipient.letter}\n\n"
+        f"Условия игры:\n"
+        f"Ограничение стоимости подарка: {game.budget}\n"
+        f"Подарок нужно отправить {game.gift_send_date.strftime('%d.%m.%Y')}."
     )
     return text
 
